15217e7945e5c34f260f282a18bcfe3ef8e26fd7
MKarim10 <mk10-@hotmail.com>
2017-05-30 10:48:08 +0200

    Ignoring .o

--- TP1/matrice_lineaire.c
+++ TP1/matrice_lineaire.c
@@ -1,36 +1,49 @@
 #include "matrice_lineaire.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 matrice allouer_matrice(int l, int c)
 {
   matrice m = {0, 0, NULL};
+  m.l = l; 
+  m.c = c; 
+  m.donnees = (double*)malloc(l*c*sizeof(double));
+  if(m.donnees == NULL){
+    printf("Erreur malloc matrice lineaire");
+    exit(0);
+  }
   return m;
 }
 
 void liberer_matrice(matrice m)
 {
+  if(m.donnees != NULL)
+    free(m.donnees);
 }
 
 int est_matrice_invalide(matrice m)
 {
   int resultat = 0;
+  resultat = (m.l == -1 || m.c == -1);
   return resultat;
 }
 
 double *acces_matrice(matrice m, int i, int j)
 {
   double *resultat = NULL;
+  if (i<m.l && j<m.c)
+    resultat = &(m.donnees[i*m.c] + j*sizeof(double));
   return resultat;
 }
 
 int nb_lignes_matrice(matrice m)
 {
-  int resultat = 0;
+  int resultat = m.l;
   return resultat;
 }
 
 int nb_colonnes_matrice(matrice m)
 {
-  int resultat = 0;
+  int resultat = m.c;
   return resultat;
 }
