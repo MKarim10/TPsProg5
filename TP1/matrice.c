23c1d2c9df22c6dd102a3e6ddfb8c4923591747c
MKarim10 <mk10-@hotmail.com>
2017-05-30 10:26:14 +0200

    Matrice OK

--- TP1/matrice.c
+++ TP1/matrice.c
@@ -1,19 +1,24 @@
 #include "matrice.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 matrice allouer_matrice(int l, int c)
 {
   matrice m = {0, 0, NULL};
-  int i, j;
+  int i;
   m.l = l; 
   m.c = c;
-  m.donnees = (double **) malloc(l*sizeof(double*));
-  if(m.donnees == NULL)
+  m.donnees = (double**) malloc(l*sizeof(double*));
+  if(m.donnees == NULL){
+    printf("erreur malloc : m.donnees" );
     exit(0);
+  }
   for (i=0; i<l; i++){
-    *(m.donnees) + i = (double*) malloc(c* sizeof(double));
-    if (*m.donnees + i == NULL)
+    m.donnees[i] = (double*) malloc(c*sizeof(double));
+    if (m.donnees[i] == NULL){
+      printf("erreur malloc m.donnees[i]");
       exit(0);
+    }
   }
 
   return m;
@@ -23,7 +28,7 @@ void liberer_matrice(matrice m)
 {
   int i;
   for (i=0; i<m.l; i++)
-    free(*m.donnees +i);
+    free(m.donnees[i]);
   free(m.donnees);
   
 }
@@ -31,8 +36,7 @@ void liberer_matrice(matrice m)
 int est_matrice_invalide(matrice m)
 {
   int resultat = 0;
-  if(m.l == -1)
-    resultat == 1;
+  resultat = (m.l == -1 || m.c == -1);
   return resultat;
 }
 
@@ -40,7 +44,7 @@ double *acces_matrice(matrice m, int i, int j)
 {
   double *resultat = NULL;
   if (i<m.l && j < m.c)
-    resultat = *(*m.donnees + i) +j;
+    resultat = &m.donnees[i][j];
   return resultat;
 }
 
